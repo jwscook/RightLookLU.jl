2dbd5fedf61e99f937661fa1ccaa1e6fd6a966a7
James Cook <cookjws@gmail.com>
2025-03-04 20:05:25 +0000

    chairmarks in test

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,8 @@ pinthreads(:cores)
 
 using Test
 using RightLookLU
-using BenchmarkTools
+using Chairmarks, BenchmarkTools
+using SparseMatricesCSR
 
 function test_matrix(T=Float64; ntiles=6, tilesize=5, overlap=0)
   n = ntiles * tilesize - overlap * (ntiles - 1)
@@ -18,78 +19,84 @@ function test_matrix(T=Float64; ntiles=6, tilesize=5, overlap=0)
   return sparse(mat)
 end
 
-function mybenchmark(f::F, args...; n=3) where F
+function mybenchmark(f::F, args...; n=NBENCH) where F
   return minimum([(newargs = deepcopy.(args); @elapsed f(newargs...)) for _ in 1:n])
 end
 
+const NBENCH=8
+
 using StatProfilerHTML, Profile
 function foo()
-   ntiles=64 ÷ 2
-   tilesize=256 ÷ 2
-  s = test_matrix(ComplexF64; ntiles=ntiles, tilesize=tilesize, overlap=tilesize ÷ 4)
+   ntiles=8
+   tilesize=128
+  s = test_matrix(ComplexF64; ntiles=ntiles, tilesize=tilesize, overlap=tilesize ÷ 8)
   s .+= 10 * I(size(s, 1))
+  s2 = deepcopy(s)
+  s2.nzval .= rand(ComplexF64, length(s2.nzval)) # == sparsity pattern, != values
   b = rand(ComplexF64, size(s, 1))
   x = s \ b
   sc = deepcopy(s)
-  rl = RLLU(sc, min(16, size(sc, 1)))
+  rl = RLLUMatrix(sc, min(16, size(sc, 1)))
   lurl = lu!(rl)
-  xrl = rl \ b
+  xrl = deepcopy(b)
+  ldiv!(rl, xrl)#xrl = rl \ b
   @test xrl ≈ x
   Profile.init(n=10^7, delay=1e-6)
   Profile.clear()
+  ldiv!(x, lurl, b)
   @profilehtml ldiv!(x, lurl, b)
-  #@assert false
-  s2 = test_matrix(ComplexF64; ntiles=ntiles, tilesize=tilesize, overlap=tilesize ÷ 4)
-  s2 .+= 10 * I(size(s2, 1))
-  s2 .+= sprand(ComplexF64, size(s2, 1), size(s2, 2), 0.1)
-  lu!(rl, s2)
-  ldiv!(rl, b)
+  run(`rm -rf ldiv3arg`)
+  run(`mv -f statprof ldiv3arg`)
+  lu!(lurl, s2)
+  ldiv!(lurl, b)
   Profile.clear()
-#  @profilehtml ldiv!(rl, b)
-  @profilehtml lu!(rl, s2)
-
+  @profilehtml ldiv!(lurl, b)
+  run(`rm -rf ldiv2arg`)
+  run(`mv -f statprof ldiv2arg`)
+#  @profilehtml lu!(rl, s2)
   #return
 
 @testset "rightlooklu!" begin
-  #for n in (128,256), lutiles in (8, 16, 32)
-  #  lutiles > n && continue
-  #  A = rand(ComplexF64, n, n)
-  #  L, U = lu(A, NoPivot())
-  #  Ac = deepcopy(A)
-  #  LR = RLLU(Ac, lutiles)
-  #  lu!(LR)
-  #  L1, U1 = tril(LR.A, -1) .+ I(n), triu(LR.A)
-  #  @test L1 * U1 ≈ A
-  #  t1 = mybenchmark((x...)->lu!(RLLU(x...)), A, lutiles; n=3)
-  #  t2 = mybenchmark(x->lu!(x, NoPivot()), A; n=3)
-  #  @show n, lutiles, t1 / t2
-  #  @benchmark lu!(RLLU($A, $lutiles))
-  #end
-  for ntiles in (4, 8, 16), tilesize in (16, 32, 64), overlap in (tilesize ÷ 4,)
-    s = test_matrix(ComplexF64; ntiles=ntiles, tilesize=tilesize, overlap=overlap)
+  for ntiles in (16,32), tilesize in (32,64,128), overlap in (tilesize ÷ 8,)
+    s = test_matrix(Float64; ntiles=ntiles, tilesize=tilesize, overlap=overlap)
     s .+= 10 * I(size(s, 1))
+    sc = deepcopy(s)
     lu_s = lu(deepcopy(s))
-    tb1 = mybenchmark(x->lu!(x, NoPivot()), s; n=20)
-    tb2 = mybenchmark(x->lu!(lu_s, x), s; n=20)
-    b = rand(ComplexF64, size(s, 1))
-    tb3 = mybenchmark(x->x \ b, lu_s; n=20)
-    for lutiles in unique(min.(size(s, 1), (4, 7, 8, 16, 17, 32, 64)))
+#    tb1 = (@benchmark lu!($s, NoPivot())).times |> minimum
+    tb1 = (@b lu!($s, NoPivot())).time
+#    tb2 = (@benchmark lu!($lu_s, $sc)).times |> minimum
+    tb2 = (@b lu!($lu_s, $sc)).time
+    b = rand(Float64, size(s, 1))
+#    tb3 = (@benchmark $lu_s \ $b).times |> minimum
+    tb3 = (@b $lu_s \ $b).time
+    for lutiles in unique(min.(size(s, 1), (16, 32, 64)))
       lutiles > size(s, 1) && continue
       sc = deepcopy(s)
-      LR = RLLU(sc, lutiles)
+      LR = RLLUMatrix(sc, lutiles)
       lulrlu = deepcopy(lu!(LR))
       L2, U2 = tril(LR.A, -1) .+ I(size(LR.A, 1)), triu(LR.A)
       #@show norm(L2 * U2 - s) / norm(s)
       @test L2 * U2 ≈ s
-      ta1 = mybenchmark((x...)->lu!(RLLU(x...)), s, lutiles; n=3)
-      ta2 = mybenchmark(x->lu!(LR, x), s; n=3)
-  #    @show ntiles, tilesize, overlap, lutiles, ta1 / tb1, ta2 / tb2
+      #ta1 = (@benchmark lu!(RLLUMatrix($s, $lutiles))).times |> minimum
+      ta1 = (@b lu!(RLLUMatrix($s, $lutiles))).time
+      #ta2 = (@benchmark lu!($lulrlu, $s)).times |> minimum
+      ta2 = (@b lu!($lulrlu, $s)).time
       transpose!(lulrlu)
       x = s \ b
       @test x ≈ lulrlu \ b
-      #@show norm(x - lulrlu \ b)
-      ta3 = mybenchmark(x->x \ b, lulrlu; n=3)
-      @show ntiles, tilesize, overlap, lutiles, ta1 / tb1, ta2 / tb2, ta3 / tb3
+      #ta3 = (@benchmark $lulrlu \ $b).times |> minimum
+      ta3 = (@b $lulrlu \ $b).time
+      Lcsc, Ucsc = lu(sc, NoPivot())
+#      Lmatrix[diagind(LMatrix)] .= 1
+#      Umatrix = UpperTriangular(Matrix(lulrlu.A[:, :]))
+      #Lcsr, Ucsr = SparseMatrixCSR(Lcsc), SparseMatrixCSR(Ucsc)
+      ta4 = (@b ($Ucsc \ ($Lcsc \ $b))).time
+      #ta5 = (@b ($Ucsr \ ($Lcsr \ $b))).time
+      #ta6 = (@b ($Ucsr \ ($Lcsc \ $b))).time
+      #ta7 = (@b ($Ucsc \ ($Lcsr \ $b))).time
+      n = size(s, 1)
+      @show n, ntiles, tilesize, overlap, lutiles
+      @show ta1 / tb1, ta2 / tb2, ta3 / tb3, ta4 / tb3#, ta5 / tb3, ta6 / tb3, ta7 / tb3
     end
   end
 
